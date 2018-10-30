3902073ff727c56bb0c8703251c9b2594ea0cc90
Steven Pears <steven.pears@live.co.uk>
2018-10-30 19:57:11 +0000

    Interface and tests (#1)

---
+++ Alexa.NET.APL.Tests/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Alexa.NET.APL.Tests
+{
+    public static class Utility
+    {
+        private const string ExamplesPath = "Examples";
+
+        public static bool CompareJson(object actual, string expectedFile)
+        {
+            var actualJObject = JObject.FromObject(actual);
+            var expected = ExampleFileContent(expectedFile);
+            var expectedJObject = JObject.Parse(expected);
+            Console.WriteLine(actualJObject);
+            return JToken.DeepEquals(expectedJObject, actualJObject);
+        }
+
+        public static T ExampleFileContent<T>(string expectedFile)
+        {
+            using (var reader = new JsonTextReader(new StringReader(ExampleFileContent(expectedFile))))
+            {
+                return new JsonSerializer().Deserialize<T>(reader);
+            }
+        }
+
+        public static string ExampleFileContent(string expectedFile)
+        {
+            return File.ReadAllText(Path.Combine(ExamplesPath, expectedFile));
+        }
+    }
+}
---
+++ Alexa.NET.APL/APLInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Request
+{
+    public class APLInterface
+    {
+        public const string InterfaceName = "Alexa.Presentation.APL";
+
+        public static bool Supported(SkillRequest request)
+        {
+            return request.Context.System.Device.SupportedInterfaces.ContainsKey(InterfaceName);
+        }
+    }
+}
