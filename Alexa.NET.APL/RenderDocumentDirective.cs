f7c9d9ea5743be00f9b506f490855ee1c15fe106
Steven Pears <steven.pears@live.co.uk>
2018-10-30 20:55:25 +0000

    Top level renderdocument directive

---
+++ Alexa.NET.APL/RenderDocumentDirective.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Response.APL;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Response
+{
+    public class RenderDocumentDirective:IDirective
+    {
+        [JsonProperty("type")]
+        public string Type => "Alexa.Presentation.APL.RenderDocument";
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("document")]
+        public APLDocument Document { get; set; }
+
+        [JsonProperty("dataSources")]
+        public Dictionary<string,object> DataSources { get; set; }
+    }
+}
