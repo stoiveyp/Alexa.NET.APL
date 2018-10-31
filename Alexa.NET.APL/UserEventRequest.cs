77cfc1d8b4f1cecab7c8bd82f1b6858469e6223c
Steven Pears <steven.pears@live.co.uk>
2018-10-31 19:33:34 +0000

    Add initial request type (#10)

---
+++ Alexa.NET.APL/UserEventRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Alexa.NET.APL
+{
+    public class UserEventRequest: Request.Type.Request
+    {
+        public const string RequestType = "Alexa.Presentation.APL.UserEvent";
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("arguments")]
+        public Dictionary<string,object> Arguments { get; set; }
+
+
+    }
+}
---
+++ Alexa.NET.APL/UserEventRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alexa.NET.APL;
+using Alexa.NET.Request.Type;
+
+namespace Alexa.NET.Request
+{
+    public class UserEventRequestHandler:IRequestTypeConverter
+    {
+        public bool CanConvert(string requestType)
+        {
+            return requestType == UserEventRequest.RequestType;
+        }
+
+        public Request.Type.Request Convert(string requestType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToRequestConverter()
+        {
+            if (RequestConverter.RequestConverters.Where(rc => rc != null).All(rc => rc.GetType() != typeof(UserEventRequestHandler)))
+            {
+                RequestConverter.RequestConverters.Add(this);
+            }
+        }
+    }
+}
