c3e5c81dc5f720b6afc16ec87c86a109f319a6a5
Steven Pears <steven.pears@live.co.uk>
2018-10-31 12:53:39 +0000

    Add ExecuteCommands directive

---
+++ Alexa.NET.APL/ExecuteCommandsDirective.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Alexa.NET.APL
+{
+    public class ExecuteCommandsDirective:IDirective
+    {
+        public ExecuteCommandsDirective() { }
+
+        public ExecuteCommandsDirective(string token)
+        {
+            Token = token;
+        }
+
+        public ExecuteCommandsDirective(string token, IEnumerable<APLCommand> commands):this(token)
+        {
+            Commands = commands.ToList();
+        }
+
+        [JsonProperty("type")]
+        public string Type => "Alexa.Presentation.APL.ExecuteCommands";
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("commands")]
+        public IList<APLCommand> Commands { get; set; }
+    }
+}
