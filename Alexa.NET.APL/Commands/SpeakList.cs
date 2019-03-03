8cbf8bda8b7fadea2844f67b0daac73b6ff48c4e
Steven Pears <steven.pears@live.co.uk>
2019-03-03 00:01:46 +0000

    Add speaklist (#35)
    update package details

---
+++ Alexa.NET.APL/Commands/SpeakList.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.APL.Commands
+{
+    public class SpeakList : APLCommand
+    {
+        public override string Type => nameof(SpeakList);
+
+        [JsonProperty("align", NullValueHandling = NullValueHandling.Ignore)]
+        public APLValue<string> Align { get; set; }
+
+        [JsonProperty("componentId")]
+        public APLValue<string> ComponentId { get; set; }
+
+        [JsonProperty("start")]
+        public APLValue<int> Start { get; set; }
+
+        [JsonProperty("count")]
+        public APLValue<int> Count { get; set; }
+
+        [JsonProperty("minimumDwellTime",NullValueHandling = NullValueHandling.Ignore)]
+        public APLValue<int> MinimumDwellTime { get; set; }
+
+    }
+}
