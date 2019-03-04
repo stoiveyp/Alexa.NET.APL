b8f9fc965a9649559aa8cec132312880835cbe3d
Steven Pears <steven.pears@live.co.uk>
2019-03-04 08:51:19 +0000

    Layouts (#36)
    * Add AlexaHeader and AlexaFooter support
    
    * Update package details

--- Alexa.NET.APL.Tests/LayoutTests.cs
+++ Alexa.NET.APL.Tests/LayoutTests.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Alexa.NET.APL.Layouts;
 using Alexa.NET.Response;
 using Alexa.NET.Response.APL;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Alexa.NET.APL.Tests
@@ -26,6 +28,50 @@ namespace Alexa.NET.APL.Tests
             var first = layout.Parameters.First();
             Assert.Equal("title",first.Name);
             Assert.Equal(ParameterType.@string,first.Type);
+        }
+
+        [Fact]
+        public void AlexaFooterAddsImport()
+        {
+            var document = new APLDocument();
+            AlexaFooter.ImportInto(document);
+            document.MainTemplate = new Layout(
+                new AlexaFooter("Hint Text")
+            ).AsMain();
+            Assert.Contains(Import.AlexaLayouts,document.Imports);
+        }
+
+        [Fact]
+        public void AlexaFooterRecognisesExistingImport()
+        {
+            var document = new APLDocument {Imports = new List<Import> {Import.AlexaLayouts}};
+            AlexaFooter.ImportInto(document);
+            Assert.Single(document.Imports);
+        }
+
+        [Fact]
+        public void AlexaFooterGeneratesCorrectJson()
+        {
+            var footer = new AlexaFooter("Hint Text");
+            Assert.True(Utility.CompareJson(footer,"AlexaFooter.json"));
+        }
+
+        [Fact]
+        public void AlexaHeaderGeneratesCorrectJson()
+        {
+            var header = new AlexaHeader
+            {
+                HeaderTitle = "Header Title",
+                HeaderSubtitle = "Header Subtitle",
+                HeaderAttributionImage = "http://HeaderAttributionImage",
+                HeaderBackgroundColor = "red",
+                HeaderBackButton = true,
+                HeaderNavigationAction = "Go Back"
+            };
+
+
+
+            Assert.True(Utility.CompareJson(header,"AlexaHeader.json"));
         }
     }
 }
---
+++ Alexa.NET.APL/Layouts/AlexaFooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Alexa.NET.APL.Components;
+using Alexa.NET.Response.APL;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.APL.Layouts
+{
+    public class AlexaFooter:CustomComponent
+    {
+        public static void ImportInto(APLDocument document)
+        {
+            Import.AlexaLayouts.ImportInto(document);
+        }
+
+        public AlexaFooter():base(nameof(AlexaFooter)) { }
+
+        public AlexaFooter(string hintText) : this()
+        {
+            HintText = hintText;
+        }
+
+        [JsonProperty("hintText",NullValueHandling = NullValueHandling.Ignore)]
+        public APLValue<string> HintText { get; set; }
+    }
+}
