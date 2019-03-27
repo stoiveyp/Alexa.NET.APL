ccbcdadcc916d700d213e304bff5882ac0716513
Steven Pears <steven.pears@live.co.uk>
2019-03-27 20:31:39 +0000

    Addition of SetState command

---
+++ Alexa.NET.APL/Commands/SetState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Alexa.NET.APL.Commands
+{
+    public class SetState:APLCommand
+    {
+        public override string Type => nameof(SetState);
+
+        [JsonProperty("componentId", NullValueHandling = NullValueHandling.Ignore)]
+        public APLValue<string> ComponentId { get; set; }
+
+        [JsonProperty("state"),JsonConverter(typeof(StringEnumConverter))]
+        public APLValue<SetStateStates> State { get; set; }
+
+        [JsonProperty("value")]
+        public APLValue<bool> Value { get; set; }
+    }
+}
