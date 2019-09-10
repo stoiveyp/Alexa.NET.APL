9445cf73b68d537c82d61c711091b33871483226
Steven Pears <steven.pears@live.co.uk>
2019-09-10 08:44:17 +0100

    Add AnimatedTransform helper methods

--- Alexa.NET.APL/Commands/AnimatedTransform.cs
+++ Alexa.NET.APL/Commands/AnimatedTransform.cs
@@ -14,5 +14,35 @@ namespace Alexa.NET.APL.Commands
 
         [JsonProperty("to", NullValueHandling = NullValueHandling.Ignore)]
         public APLValue<IList<APLTransform>> To { get; set; }
+
+        public IList<AnimatedProperty> Multiple(IEnumerable<APLTransform> from, IEnumerable<APLTransform> to)
+        {
+            return new List<AnimatedProperty>
+                {
+                    new AnimatedTransform
+                    {
+                        From = new List<APLTransform>(from),
+                        To = new List<APLTransform>(to)
+                    }
+                };
+        }
+
+        public IList<AnimatedProperty> Single(APLTransform from, APLTransform to)
+        {
+            return new List<AnimatedProperty>
+            {
+                new AnimatedTransform
+                {
+                    From = new List<APLTransform>
+                    {
+                        from
+                    },
+                    To = new List<APLTransform>
+                    {
+                        to
+                    }
+                }
+            };
+        }
     }
 }
