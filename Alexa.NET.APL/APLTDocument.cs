b1f8566f0bc9f12675b5393d3cb978462c394d62
Steven Pears <steven.pears@live.co.uk>
2019-10-23 21:21:46 +0100

    Get APLDocumentBase with working tests

---
+++ Alexa.NET.APL/APLTDocument.cs
@@ -0,0 +1,14 @@
+﻿using Alexa.NET.APL;
+
+namespace Alexa.NET.Response.APL
+{
+    public class APLTDocument : APLDocumentBase
+    {
+        public override string Type => "APLT";
+
+        public APLTDocument():base(APLDocumentVersion.V1)
+        {
+
+        }
+    }
+}
