a6f9e6963c888085b3de096bec2a8ced3614d053
Steven Pears <steven.pears@live.co.uk>
2019-10-30 20:50:44 +0000

    Add APLTInterface helper methods

---
+++ Alexa.NET.APL/APLTInterface.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.Request
+{
+    public static class APLTInterface
+    {
+        public const string InterfaceName = "Alexa.Presentation.APLT";
+
+        public static bool APLTSupported(this SkillRequest request)
+        {
+            return Supported(request);
+        }
+
+        public static bool Supported(SkillRequest request)
+        {
+            return request.Context.System.Device.SupportedInterfaces.ContainsKey(InterfaceName);
+        }
+
+        public static APLInterfaceDetails APLTInterfaceDetails(this SkillRequest request)
+        {
+            var raw = GetAPLTInterfaceObject(request);
+            return raw == null ? null : JsonConvert.DeserializeObject<APLInterfaceDetails>(raw.ToString());
+        }
+
+        private static object GetAPLTInterfaceObject(SkillRequest request)
+        {
+            var interfaces = request.Context.System.Device.SupportedInterfaces;
+            return interfaces.ContainsKey(InterfaceName) ? interfaces[InterfaceName] : null;
+        }
+    }
+}
