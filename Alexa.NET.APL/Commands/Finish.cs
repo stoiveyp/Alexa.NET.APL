f01c53cb790c5d92caca96e245bcaa911b06f045
Steven Pears <steven.pears@live.co.uk>
2020-03-26 23:17:49 +0000

    Add APL 1.3 Commands (#79)
    * Add APL 1.3 Commands

---
+++ Alexa.NET.APL/Commands/Finish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.APL.Commands
+{
+    public class Finish:APLCommand
+    {
+        public override string Type => "Finish";
+    }
+}
---
+++ Alexa.NET.APL/Commands/Select.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.APL.JsonConverter;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.APL.Commands
+{
+    public class Select:APLCommand
+    {
+        public override string Type => "Select";
+
+        [JsonProperty("commands", NullValueHandling = NullValueHandling.Ignore),
+         JsonConverter(typeof(APLCommandListConverter))]
+        public APLValue<IList<APLCommand>> Commands { get; set; }
+
+        [JsonProperty("otherwise", NullValueHandling = NullValueHandling.Ignore),
+         JsonConverter(typeof(APLCommandListConverter))]
+        public APLValue<IList<APLCommand>> Otherwise { get; set; }
+
+        [JsonProperty("data",NullValueHandling = NullValueHandling.Ignore)]
+        public object[] Data { get; set; }
+    }
+}
