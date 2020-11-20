74ae5572fd11e432ba2bb4f919a831921355fea2
Steven Pears <steven.pears@live.co.uk>
2020-11-20 18:15:42 +0000

    Gradient and color image filters

---
+++ Alexa.NET.APL/Filters/Color.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.APL.Filters
+{
+    public class Color : IImageFilter
+    {
+        [JsonProperty("type")]
+        public string Type => nameof(Color);
+
+        [JsonProperty("color",NullValueHandling = NullValueHandling.Ignore)]
+        public APLValue<string> SelectedColor { get; set; }
+    }
+}
