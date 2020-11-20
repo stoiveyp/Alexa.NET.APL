e11a928d3dd13ce5b9ddd3da8d8974a31a254522
Steven Pears <steven.pears@live.co.uk>
2020-11-20 18:10:21 +0000

    Add blend filter

--- Alexa.NET.APL/Filters/Noise.cs
+++ Alexa.NET.APL/Filters/Noise.cs
@@ -5,14 +5,16 @@ namespace Alexa.NET.APL.Filters
 {
     public class Noise : IImageFilter
     {
+        [JsonProperty("type")]
         public string Type => nameof(Noise);
-        [JsonProperty("useColor",NullValueHandling = NullValueHandling.Ignore)]
+
+        [JsonProperty("useColor", NullValueHandling = NullValueHandling.Ignore)]
         public APLValue<bool?> UseColor { get; set; }
 
-        [JsonProperty("sigma",NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("sigma", NullValueHandling = NullValueHandling.Ignore)]
         public APLValue<int?> Sigma { get; set; }
 
-        [JsonProperty("kind",NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("kind", NullValueHandling = NullValueHandling.Ignore)]
         [JsonConverter(typeof(APLValueEnumConverter<NoiseKind>))]
         public APLValue<NoiseKind?> Kind { get; set; }
     }
