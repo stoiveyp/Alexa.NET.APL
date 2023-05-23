9e0ee44c96dd7718f21e3f2d429eeeade667ea50
Steven Pears <steven.pears@live.co.uk>
2023-05-23 19:26:59 +0100

    API client and cancel method

---
+++ Alexa.NET.APL.Tests/DataStoreClientTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Alexa.NET.APL.DataStore;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Alexa.NET.APL.Tests
+{
+    public class DataStoreClientTests
+    {
+        [Fact]
+        public async Task AccessTokenAsksForTheCorrectToken()
+        {
+            var client = new AccessTokenClient(new HttpClient(new ActionHandler(async hr => 
+            {
+                Assert.Equal("https://api.amazon.com/auth/O2/token",hr.RequestUri.ToString());
+                var content = await hr.Content.ReadAsStringAsync();
+                Assert.Equal("client_id=x&client_secret=y&grant_type=client_credentials&scope=alexa%3A%3Adatastore", content);
+
+            },new JObject(
+                new JProperty("access_token","xxx"),
+                new JProperty("expires_in",3600),
+                new JProperty("scope", "alexa::datastore"),
+                new JProperty("token_type", "bearer")
+                ))));
+
+            var result = await client.Send("x", "y");
+            Assert.Equal("xxx", result.Token);
+            Assert.Equal("alexa::datastore", result.Scope);
+        }
+
+        [Fact]
+        public async Task CancelMethodSendsCorrectly()
+        {
+            var client = new DataStoreClient(new HttpClient(new ActionHandler(async hr =>
+            {
+                Assert.Equal("https://example.com/v1/datastore/queue/x/cancel", hr.RequestUri.ToString());
+                Assert.Equal("application/json",hr.Content.Headers.ContentType.MediaType);
+
+            }, HttpStatusCode.NoContent)),"https://example.com","xxx");
+
+            var result = await client.Cancel("x");
+            Assert.True(result);
+        }
+    }
+}
---
+++ Alexa.NET.APL/DataStore/DataStoreClient.cs
@@ -0,0 +1,47 @@
+﻿using Alexa.NET.Request;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alexa.NET.APL.DataStore
+{
+    public class DataStoreClient
+    {
+        public HttpClient Client { get; }
+        public Uri BaseAddress { get; }
+        private string Token { get; }
+
+        public DataStoreClient(SkillRequest request) : this(
+            request.Context.System.ApiEndpoint,
+            request.Context.System.ApiAccessToken)
+        {
+        }
+
+        public DataStoreClient(string endpointUrl, string accessToken):this(null, endpointUrl, accessToken)
+        {
+            
+        }
+
+        public DataStoreClient(HttpClient client, string endpointUrl, string accessToken)
+        {
+            Client = client ?? new HttpClient();
+            BaseAddress = new Uri(endpointUrl);
+            Token = accessToken;
+        }
+
+        public async Task<bool> Cancel(string queuedResultId)
+        {
+            var url = $"/v1/datastore/queue/{queuedResultId}/cancel";
+            var msg = new HttpRequestMessage(HttpMethod.Post, new Uri(BaseAddress, url))
+            {
+                Content = new StringContent(string.Empty, Encoding.UTF8, "application/json")
+            };
+            var response = await Client.SendAsync(msg);
+            return response.StatusCode == HttpStatusCode.NoContent;
+        }
+    }
+}
