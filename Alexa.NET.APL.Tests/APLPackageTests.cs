ec684c716ee8805ba679440997654ecfe9230313
Steven Pears <steven.pears@live.co.uk>
2023-05-29 18:05:25 +0100

    Add APL package manifest support

---
+++ Alexa.NET.APL.Tests/APLPackageTests.cs
@@ -0,0 +1,14 @@
+﻿using Alexa.NET.APL.Package;
+using Xunit;
+
+namespace Alexa.NET.APL.Tests
+{
+    public class APLPackageTests
+    {
+        [Fact]
+        public void ValidPackageDocument()
+        {
+            Utility.AssertSerialization<APLPackage>("APLPackage.json");
+        }
+    }
+}
